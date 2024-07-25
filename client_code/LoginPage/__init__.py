195f892db363e990a6e954f313c4c62ed8369c12
ryan@anvil.works <ryan@anvil.works>
2024-07-25 10:37:49 +0000

    Edited settings and LoginPage

--- client_code/LoginPage/__init__.py
+++ client_code/LoginPage/__init__.py
@@ -6,7 +6,7 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 
-from ...M3_App_28.Calculator import Calculator
+from ..Calculator import Calculator
 
 class LoginPage(LoginPageTemplate):
   def __init__(self, **properties):
@@ -24,11 +24,11 @@ class LoginPage(LoginPageTemplate):
   def form_show(self, **event_args):
     """This method is called when the HTML panel is shown on the screen"""
     if anvil.users.get_user():
-      self.outlined_card_1.visible = False
+      self.card_1.visible = False
       open_form('calculator')
     else:
       # Stops the glitch in rendering components if we're only going to open the main form anyway
-      self.outlined_card_1.visible = True
+      self.card_1.visible = True
       
       # TEMPLATE EXPLANATION ONLY - DELETE ROWS 31-32 WHEN YOU'RE READY
       Notification("Here's your SaaS app's login page. To start click login and then signup for an account.", title="Template Explanation", timeout=None, style="warning").show()
