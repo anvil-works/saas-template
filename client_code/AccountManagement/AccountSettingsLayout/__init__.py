5c3853c685b3274e30af06109ba594b5ce040e5f
ryan@anvil.works <ryan@anvil.works>
2024-10-08 14:18:07 +0000

    Edited AccountManagement.AccountSettingsLayout

--- client_code/AccountManagement/AccountSettingsLayout/__init__.py
+++ client_code/AccountManagement/AccountSettingsLayout/__init__.py
@@ -13,3 +13,12 @@ class AccountSettingsLayout(AccountSettingsLayoutTemplate):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  
+  def account_tab_button_click(self, **event_args):
+      """This method is called when the component is clicked."""
+      alert(AccountPanel(), title=anvil.users.get_user()["email"], dismissible=True, buttons=None, large=True)
+
+  def subscription_tab_button_click(self, **event_args):
+    """This method is called when the component is clicked."""
+    alert(SubscriptionPanel())
